44e69b4bed71cb13dbee588e34398d98a2c984de
DARSHAN V G <darshanguna22@gmail.com>
2026-02-05 14:37:53 +0530

    added clg

--- next-app/src/app/CollegeList.js
+++ next-app/src/app/CollegeList.js
@@ -217,6 +217,7 @@ const colleges = [
   `M.A.M. College of Engineering, Siruganur, Tiruchirappalli District 621105`,
   `M.A.M. School of Engineering, Siruganur, Tiruchirappalli District 621105`,
   `Madha Engineering College, Kundrathur, Chennai 600069`,
+  'Avinashilingam Institute for Home Science and Higher Education for Women, Coimbatore-641 043',
   `Sri Ramakrishna Engineering College`,
   `Madha Institute of Engineering and Technology, Erandamkattalai Village, Sadhananthapuram, Thandalam Post, Chennai 602101`,
   `Madurai Institute of Engineering & Technology, Pottapalayam Village, Manamadurai Taluk, Sivagangai District 630611`,
